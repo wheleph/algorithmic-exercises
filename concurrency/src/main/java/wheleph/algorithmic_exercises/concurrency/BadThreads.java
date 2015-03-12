0ac1807d9f71d26e57362d15b2901d3983a61e95
wheleph <wheleph@gmail.com>
2015-03-12 16:26:30 +0200

    Initial problem statement from concurrency tutorial trail

---
+++ concurrency/src/main/java/wheleph/algorithmic_exercises/concurrency/BadThreads.java
@@ -0,0 +1,36 @@
+package wheleph.algorithmic_exercises.concurrency;
+
+/**
+ * Problem: The application should print out "Mares do eat oats." Is it guaranteed to always do this?
+ * If not, why not? Would it help to change the parameters of the two invocations of Sleep?
+ * How would you guarantee that all changes to message will be visible in the main thread?
+ */
+public class BadThreads {
+
+    static String message;
+
+    private static class CorrectorThread
+            extends Thread {
+
+        public void run() {
+            try {
+                sleep(1000);
+            } catch (InterruptedException e) {
+            }
+
+            // Key statement 1:
+            message = "Mares do eat oats.";
+        }
+    }
+
+    public static void main(String args[])
+            throws InterruptedException {
+
+        (new CorrectorThread()).start();
+        message = "Mares do not eat oats.";
+        Thread.sleep(2000);
+
+        // Key statement 2:
+        System.out.println(message);
+    }
+}
